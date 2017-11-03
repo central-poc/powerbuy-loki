e9715982f5c13bbb560ef6fa2d609ba1d321e1bd
Agent Ruang <ruen.dev@gmail.com>
2017-11-03 13:34:53 +0700

    modify product's extension attributes

--- app/code/Powerbuy/Catalog/Plugin/Magento/Catalog/Model/ProductRepository.php
+++ app/code/Powerbuy/Catalog/Plugin/Magento/Catalog/Model/ProductRepository.php
@@ -1,7 +1,11 @@
 <?php
 namespace Powerbuy\Catalog\Plugin\Magento\Catalog\Model;
 
+use Magento\Catalog\Api\Data\ProductExtensionFactory;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\Data\ProductSearchResultsInterface;
 use Magento\Eav\Model\Config;
+use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\Webapi\Rest\Request as RestRequest;
 use Powerbuy\Catalog\Model\ResourceModel\Product as ProductResource;
 use Powerbuy\Promotion\Model\ResourceModel\Promotion as PromotionResource;
@@ -12,21 +16,36 @@ class ProductRepository
     private $restRequest;
 
     protected $eavConfig;
+    /**
+     * @var ProductExtensionFactory
+     */
+    private $extensionFactory;
 
+    /**
+     * ProductRepository constructor.
+     * @param RestRequest $restRequest
+     * @param ProductResource $productResource
+     * @param PromotionResource $promotionResource
+     * @param Config $eavConfig
+     * @param ProductExtensionFactory $extensionFactory
+     */
     public function __construct(
         RestRequest $restRequest,
         ProductResource $productResource,
         PromotionResource $promotionResource,
-        Config $eavConfig
+        Config $eavConfig,
+        ProductExtensionFactory $extensionFactory
     )
     {
         $this->restRequest = $restRequest;
         $this->productResource = $productResource;
         $this->promotionresource = $promotionResource;
         $this->eavConfig = $eavConfig;
+        $this->extensionFactory = $extensionFactory;
     }
 
-    function beforeGetList($subject, $result)
+    
+    function beforeGetList($subject,  $result)
     {
         $branchId = 0;
         foreach ($result->getFilterGroups() as $group) {
@@ -54,16 +73,19 @@ class ProductRepository
         return [$result];
     }
 
-    function afterGetList($subject, $result)
+
+    function afterGetList($subject,  $result)
     {
         foreach ($result->getItems() as $product) {
+            $this->setExtensionProductDescription($product);
+            $this->setExtensionProductImage($product);
             $this->setExtensionProductByStore($product, $this->storeCode);
         }
 
         return $result;
     }
 
-    function afterGet($subject, $result)
+    function afterGet($subject, ProductInterface $result)
     {
         $requestParams = $this->restRequest->getParams();
         if (array_key_exists('branch_id', $requestParams)) {
@@ -72,16 +94,42 @@ class ProductRepository
         }else{
             $storeId = '00099';
         }
+        $this->setExtensionProductDescription($result);
+        $this->setExtensionProductImage($result);
         $this->setExtensionProductByStore($result, $storeId);
         $this->setExtensionPromotionByProduct($result);
         return $result;
     }
 
-    private function setExtensionProductByStore($product, $storeId)
+    private function setExtensionProductDescription(ProductInterface $product)
     {
         $extensionAttributes = $product->getExtensionAttributes();
         if (empty($extensionAttributes)) {
-            $extensionAttributes = $this->productExtensionFactory->create();
+            $extensionAttributes = $this->extensionFactory->create();
+        }
+        $description = $product->getCustomAttribute('description');
+        $extensionAttributes->setDescription($description);
+        $product->setExtensionAttributes($extensionAttributes);
+        return $product;
+    }
+
+    private function setExtensionProductImage(ProductInterface $product)
+    {
+        $extensionAttributes = $product->getExtensionAttributes();
+        if (empty($extensionAttributes)) {
+            $extensionAttributes = $this->extensionFactory->create();
+        }
+        $image = $product->getCustomAttribute('image');
+        $extensionAttributes->setDescription($image);
+        $product->setExtensionAttributes($extensionAttributes);
+        return $product;
+    }
+
+    private function setExtensionProductByStore(ProductInterface $product, $storeId)
+    {
+        $extensionAttributes = $product->getExtensionAttributes();
+        if (empty($extensionAttributes)) {
+            $extensionAttributes = $this->extensionFactory->create();
         }
 
         $storeDetail = $this->productResource->getProductByStore($product['sku'], $storeId);
@@ -91,11 +139,11 @@ class ProductRepository
         return $product;
     }
 
-    private function setExtensionPromotionByProduct($product)
+    private function setExtensionPromotionByProduct(ProductInterface $product)
     {
         $extensionAttributes = $product->getExtensionAttributes();
         if (empty($extensionAttributes)) {
-            $extensionAttributes = $this->productExtensionFactory->create();
+            $extensionAttributes = $this->extensionFactory->create();
         }
         $promotion = $this->promotionresource->getPromotionByProduct($product['sku']);
         $extensionAttributes->setPromotion($promotion);
