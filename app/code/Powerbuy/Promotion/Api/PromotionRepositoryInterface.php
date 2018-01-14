60db8fcb5ea068b237c3811f5bdaf74a704c9777
Ruang <ruen.dev@gmail.com>
2018-01-14 12:03:12 +0700

    Add Interface commet

--- app/code/Powerbuy/Promotion/Api/PromotionRepositoryInterface.php
+++ app/code/Powerbuy/Promotion/Api/PromotionRepositoryInterface.php
@@ -7,16 +7,39 @@ use Magento\Framework\Api\SearchCriteriaInterface;
 
 interface PromotionRepositoryInterface 
 {
+    /**
+     * @param PromotionInterface $page
+     * @return mixed
+     */
     public function save(PromotionInterface $page);
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getById($id);
 
+    /**
+     * @param SearchCriteriaInterface $criteria
+     * @return mixed
+     */
     public function getList(SearchCriteriaInterface $criteria);
 
+    /**
+     * @param PromotionInterface $page
+     * @return mixed
+     */
     public function delete(PromotionInterface $page);
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function deleteById($id);
 
+    /**
+     * @return mixed
+     */
     public function importPro();
 
 }
